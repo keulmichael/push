9418bf0542ac4a8c61164969aa0298702f2bc883
Host <host.michael@gmail.com>
2015-09-05 14:43:41 +0200

    Update index.js

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -4,25 +4,25 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady (){alert('deviceready');
 
 
-//var pushNotification = window.plugins.pushNotification;
+var pushNotification = window.plugins.pushNotification;
 
-//pushNotification.register(
-//    successHandler, 
-//    errorHandler, 
-//    {
- //       'senderID':'651586884852',
-//        'ecb':'onNotificationGCM' // callback function
- //   }
-//);
-//    },
+pushNotification.register(
+    successHandler, 
+    errorHandler, 
+    {
+      'senderID':'651586884852',
+        'ecb':'onNotificationGCM' // callback function
+    }
+);
+    },
 
-//    function successHandler(result) {
-//    alert('Success: '+ result);
-//}
+    function successHandler(result) {
+    alert('Success: '+ result);
+}
 
 
-//function errorHandler(error) {
-//    alert('Error: '+ error);
-//}
+function errorHandler(error) {
+    alert('Error: '+ error);
+}
 }
 
